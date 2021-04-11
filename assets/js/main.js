7d44c98e645aa7dd4529ef72fa30e89644a47f00
maiobarbero <66015486+maiobarbero@users.noreply.github.com>
2021-04-11 18:55:45 +0200

    2. dark mode in local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,18 +8,22 @@
 		//Switch Nigth Mode
 		const toggleBtn = document.querySelector("#flexSwitchCheckDefault");
 		if (
-			window.matchMedia &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches
+			(window.matchMedia &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches) ||
+			localStorage.getItem("checked")
 		) {
 			document.body.setAttribute("data-theme", "dark");
 			toggleBtn.setAttribute("checked", "checked");
+			localStorage.setItem("checked", "checked");
 		}
 
 		$("#flexSwitchCheckDefault").click(function () {
 			if ($("#flexSwitchCheckDefault").is(":checked")) {
 				$("body").attr("data-theme", "dark");
+				localStorage.setItem("checked", "checked");
 			} else {
 				$("body").attr("data-theme", "light");
+				localStorage.removeItem("checked");
 			}
 		});
 	});
